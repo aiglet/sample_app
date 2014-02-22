d4508aeeb967a5de3f088c26f735e4450f994f53
Kate Secor <aiglet@gmail.com>
2014-02-21 16:20:23 -0800

    User signup updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   	@user = User.new
   end
   
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+  
   def show
   	@user = User.find(params[:id])
   end
